72c15362642d0d6d61527b6f10ddfd6636709786
sejoon00 <parksj313@konkuk.ac.kr>
2023-05-04 16:52:22 +0900

    q1

--- q1/q1.py
+++ q1/q1.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[20]:
+# In[24]:
 
 
 import csv
@@ -13,21 +13,23 @@ if __name__ == "__main__":
     min = 999
     max = -999
     sum =0
+    sum1 =0
+    sum2 =0
     count =0
     for row in data : 
+        if row[2] == '' or row[3] == '' or row[4] =='':
+            continue
         temp = float(row[2])
-        if min > temp:
-            min = temp
-        if max < temp:
-            max = temp
+        temp1 = float(row[3])
+        temp2 = float(row[4])
         sum += temp
+        sum1 += temp1
+        sum2 += temp2
         count +=1
-
-    average = round(sum/count,2)
     print("*** Annual Temperature Report for Seoul in 2022 ***")
-    print("Average Temperature:",round(average,2),"Celsius")
-    print("Average Minimum Temperature:",round(min,2),"Celsius")
-    print("Average Maximum Temperature:",round(max,2),"Celsius")
+    print("Average Temperature:",round(sum/count,2),"Celsius")
+    print("Average Minimum Temperature:",round(sum1/count,2),"Celsius")
+    print("Average Maximum Temperature:",round(sum2/count,2),"Celsius")
     f.close()
 
 
