f686c6aee21ede99d0fe3f9740a23e216a38b7c4
sejoon00 <parksj313@konkuk.ac.kr>
2023-05-04 15:42:52 +0900

    q1

--- q1/q1.py
+++ q1/q1.py
@@ -1,31 +1,34 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[10]:
+# In[20]:
 
 
 import csv
-f = open('2022_Seoul_Temp.csv','r', encoding='cp949')
-data = csv.reader(f)
-header = next(data)
-min = 999
-max = -999
-sum =0
-count =0
-for row in data : 
-    temp = float(row[2])
-    if min > temp:
-        min = temp
-    if max < temp:
-        max = temp
-    sum += temp
-    count +=1
-average = float(max/count)
-print("*** Annual Temperature Report for Seoul in 2022 ***")
-print("Average Temperature:",round(average,2),"Celsius")
-print("Average Minimum Temperature:",round(min,2),"Celsius")
-print("Average Maximum Temperature:",round(max,2),"Celsius")
-f.close()
+
+if __name__ == "__main__":
+    f = open('2022_Seoul_Temp.csv','r', encoding='cp949')
+    data = csv.reader(f)
+    header = next(data)
+    min = 999
+    max = -999
+    sum =0
+    count =0
+    for row in data : 
+        temp = float(row[2])
+        if min > temp:
+            min = temp
+        if max < temp:
+            max = temp
+        sum += temp
+        count +=1
+
+    average = round(sum/count,2)
+    print("*** Annual Temperature Report for Seoul in 2022 ***")
+    print("Average Temperature:",round(average,2),"Celsius")
+    print("Average Minimum Temperature:",round(min,2),"Celsius")
+    print("Average Maximum Temperature:",round(max,2),"Celsius")
+    f.close()
 
 
 # In[ ]:
